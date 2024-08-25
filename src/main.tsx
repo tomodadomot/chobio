a16c6081f09568afdb249a088a14fd19114afd32
chobi <tomodadomot@gmail.com>
2024-08-25 20:38:55 +0900

    replace initial setup with Vite Redux template

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import { createRoot } from "react-dom/client"
+import { Provider } from "react-redux"
+import App from "./App"
+import { store } from "./app/store"
+import "./index.css"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById("root")
+
+if (container) {
+  const root = createRoot(container)
+
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+  )
+} else {
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+  )
+}
